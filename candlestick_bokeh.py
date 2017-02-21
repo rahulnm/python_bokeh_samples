89a5b78fbddefc856aae6a7fa3716931147418ab
Rahul Mudliar <rahulnm@gmail.com>
2017-02-21 17:26:15 +0800

    Add display for resistance line.

--- candlestick_bokeh.py
+++ candlestick_bokeh.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from bokeh.plotting import output_file, figure, show
 from math import pi
+from bokeh.models import Span
 
 df = pd.read_csv(filepath_or_buffer='tsla.csv',
                  parse_dates=True)[:50]
@@ -20,6 +21,12 @@ p.grid.grid_line_alpha = 0.3
 p.segment(df.date, df.high, df.date, df.low, color="black")
 p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#a3db0a", line_color="black")
 p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#e94316", line_color="black")
+resistance_line = Span(location=21.6,
+                       dimension='width',
+                       line_color='blue',
+                       line_dash='dashed',
+                       line_width=3)
+p.add_layout(resistance_line)
 
 output_file("candlestick.html", title="candlestick.py example")
 
