4e4acfb095337d076e7fa53b3e740b56be465b0e
Rahul Mudliar <rahulnm@gmail.com>
2017-02-21 16:30:36 +0800

    Change up down color to green/red

--- candlestick_bokeh.py
+++ candlestick_bokeh.py
@@ -18,8 +18,8 @@ p.xaxis.major_label_orientation = pi / 4
 p.grid.grid_line_alpha = 0.3
 
 p.segment(df.date, df.high, df.date, df.low, color="black")
-p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
-p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
+p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#a3db0a", line_color="black")
+p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#e94316", line_color="black")
 
 output_file("candlestick.html", title="candlestick.py example")
 
